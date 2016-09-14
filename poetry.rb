a1e033242f9d083fd1d7d2e1766bf0ca94a12203
Andrew DiMichele <backflip@gmail.com>
2016-09-14 15:48:36 -0400

    Turn verbosity on

--- poetry.rb
+++ poetry.rb
@@ -7,7 +7,7 @@ Bundler.require
 RHYME_DICTIONARY_FILENAME = File.expand_path('../cmudict/cmudict-0.7b', __FILE__)
 CORPORA_PATH = File.expand_path('../corpus', __FILE__)
 CONFIG_FILENAME = File.expand_path('../config.yml', __FILE__)
-VERBOSE = false
+VERBOSE = true
 
 Dir[File.expand_path('../lib/**/*.rb', __FILE__)].each{ |file| require file }
 
